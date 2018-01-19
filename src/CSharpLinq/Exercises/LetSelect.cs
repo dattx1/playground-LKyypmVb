ef29db7baafa3b4d028d5fe228c69f829f48f177
Aimery THOMAS <thomas.aimery@avanade.com>
2018-01-19 11:48:27 +0100

    test snippet

--- src/CSharpLinq/Exercises/LetSelect.cs
+++ src/CSharpLinq/Exercises/LetSelect.cs
@@ -28,15 +28,15 @@ namespace CSharpLinq.Exercises
     //  Scandium 21  
 }
         private static List<Element> BuildList()  
-{  
-    return new List<Element>  
-    {  
-        { new Element() { Symbol="K", Name="Potassium", AtomicNumber=19}},  
-        { new Element() { Symbol="Ca", Name="Calcium", AtomicNumber=20}},  
-        { new Element() { Symbol="Sc", Name="Scandium", AtomicNumber=21}},  
-        { new Element() { Symbol="Ti", Name="Titanium", AtomicNumber=22}}  
-    };  
-}  
+        {  
+            return new List<Element>  
+            {  
+                { new Element() { Symbol="K", Name="Potassium", AtomicNumber=19}},  
+                { new Element() { Symbol="Ca", Name="Calcium", AtomicNumber=20}},  
+                { new Element() { Symbol="Sc", Name="Scandium", AtomicNumber=21}},  
+                { new Element() { Symbol="Ti", Name="Titanium", AtomicNumber=22}}  
+            };  
+        }  
     }
 
     public class Element  
